29a9e017c359567d05f5dde01ffd1a447613e847
Habenzy <robertrstauss@gmail.com>
2018-12-05 15:53:32 -0500

    merged updated master

--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -1,36 +1,14 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
-import moment from 'moment';
+import MyCalendar from '../components/my-calendar';
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <h1>Upcoming Events</h1>
-    <ul>
-      {data.allStrapiEvent.edges.map(document => (
-        <li key={document.node.id}>
-          <h2>
-            {document.node.name}
-          </h2>
-          <p>{moment(document.node.date).format("MMMM Do")}</p>
-        </li>
-      ))}
-    </ul>
+
+const eventPage = ({ data }) => (
+  <Layout>    
+    <h1>Event Calendar</h1>
+    <MyCalendar />
   </Layout>
 );
 
-export default IndexPage;
-
-export const pageQuery = graphql`
-  query EventQuery {
-    allStrapiEvent {
-      edges {
-        node {
-          id
-          name
-          date
-        }
-      }
-    }
-  }
-`;
+export default eventPage;
