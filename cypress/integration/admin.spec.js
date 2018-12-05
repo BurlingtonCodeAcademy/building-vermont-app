279bf8a941fa7193dc7cc9b11b2c5691064efba9
Leight Johnson <leightjohnson93@gmail.com>
2018-12-05 11:31:14 -0500

    add .env file for DB and admin config

--- cypress/integration/admin.spec.js
+++ cypress/integration/admin.spec.js
@@ -1,5 +1,8 @@
-describe('Admin Page', () => {
+//Set variables USER and PASS when running npm test
+describe('Logs into admin account', () => {
   it('Visits the admin page', () => {
     cy.visit('http://localhost:1337/admin');
+    cy.get('#identifier').type(process.env.USER);
+    cy.get('#password').type(process.env.PASS);
   });
 });
