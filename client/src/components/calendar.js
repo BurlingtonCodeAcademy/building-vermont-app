f66ef866c79f6ca5e52a70539ccdc2a04eb67ab3
Habenzy <robertrstauss@gmail.com>
2018-12-05 15:19:41 -0500

    map with basic style, and ability to target specific date which needs to be abstracted

--- client/src/components/calendar.js
+++ client/src/components/calendar.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import Calendar from 'react-calendar';
 
+
 class MyCalendar extends Component {
   state = {
     date: new Date(),
-    eventDate: new Date('December 25, 2018')
   }
 
   onChange = date => this.setState({date});
 
-  tileContent = ({date}) => date.getDay() === 1 ? <p>It's Monday!</p> : null; 
+  tileContent = ({date}) => date.toISOString() === new Date('December 25, 2018').toISOString() ? <p>It's Christmas!</p> : console.log(date.toISOString()); 
 
   render() {
     return (
@@ -19,9 +19,23 @@ class MyCalendar extends Component {
         value={this.state.date}
         tileContent={this.tileContent}
         />
+        <br></br>
+        <center><h4>Add an Event:</h4>
+        <p>All Events must be verified by admin before being added to the calendar</p>
+        </center>
+        <form id='event-input' action='/#' method='post'>
+          <center>
+          <input type='text' placeholder='event name'></input>
+          <input type='text' placeholder='description'></input>
+          <br></br>
+          <input type='date'></input>
+          <input type='time'></input>
+          <button type='submit'>Submit</button>
+          </center>
+        </form>
       </div>
     )
   }
-}
+};
 
-export default MyCalendar
+export default MyCalendar;
--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -3,6 +3,7 @@ import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import MyCalendar from '../components/calendar';
 
+
 const eventPage = ({ data }) => (
   <Layout>    
     <h1>Event Calendar</h1>
