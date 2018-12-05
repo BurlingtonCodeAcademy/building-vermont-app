4706c49d02f2bf09aa3011eb9f7177dbbe2bd93f
Leight Johnson <leightjohnson93@gmail.com>
2018-12-05 13:27:54 -0500

    cypress test for admin login, add/delete building

--- cypress/integration/admin.spec.js
+++ cypress/integration/admin.spec.js
@@ -1,8 +1,46 @@
-//Set variables USER and PASS when running npm test
-describe('Logs into admin account', () => {
-  it('Visits the admin page', () => {
+describe('Adds a building', () => {
+  it('Visit the admin page', () => {
     cy.visit('http://localhost:1337/admin');
-    cy.get('#identifier').type(process.env.USER);
-    cy.get('#password').type(process.env.PASS);
+  });
+  it('Enter credentials and log in', () => {
+    cy.get('#identifier').type(Cypress.env('ADMIN_USERNAME'));
+    cy.get('#password').type(Cypress.env('ADMIN_PASSWORD'));
+    cy.get('#rememberMe').uncheck();
+    cy.get('button[type="submit"').click();
+  });
+  it('Navigates to Buildings list', () => {
+    cy.get(
+      'a[href="/admin/plugins/content-manager/building?source=content-manager"]'
+    ).click();
+  });
+  it('Enter building information', () => {
+    cy.get('.content-manager button:first').click();
+    cy.get('#name').type('Test Name');
+    cy.get('#description').type('Test Description');
+    cy.get('#style').type('Test Style');
+    cy.get('#type').type('Test Type');
+    cy.get('#year').type(1776);
+    cy.get('#street').type('Test Street');
+    cy.get('#city').type('Test City');
+  });
+  it('Saves the building to the database', () => {
+    cy.get('.content-manager button:nth-child(2)').click();
+    cy.contains('Test Name');
+  });
+  it('Deletes the building', () => {
+    cy.wait(500);
+    cy.get('img[alt="filter_logo"]')
+      .eq(1)
+      .click();
+    cy.get('input[name="0.value"]').type('Test Name');
+    cy.get('.content-manager button:nth-child(2)')
+      .eq(0)
+      .click();
+    cy.get('.content-manager td i')
+      .eq(1)
+      .click();
+    cy.get('.modal-content button')
+      .eq(2)
+      .click();
   });
 });
