eaaa4844f19fad55f44aa31a5577c4ad783e5e9c
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-12-06 14:38:46 -0500

    fixed architect template

--- client/src/templates/architect.js
+++ client/src/templates/architect.js
@@ -4,14 +4,17 @@ import Layout from '../components/layout';
 
 const ArchitectTemplate = ({ data }) => (
   <Layout>
-    <h1>{data.strapiArchitect.username}</h1>
+    <h1>{data.strapiArchitect.name}</h1>
+    <p>
+    {data.strapiArchitect.bio}
+    </p>
     <ul>
-      {data.strapiArchitect.architects.map(architect => (
-        // <li key={article.id}>
+      {/* {data.strapiArchitect.architects.map(architect => (
+        <li key={article.id}>
           <h2>{architect.name}</h2>
-          // <p>{article.content}</p>
-        // </li>
-      ))}
+          <p>{article.content}</p>
+        </li>
+      ))} */}
     </ul>
   </Layout>
 );
@@ -22,6 +25,8 @@ export const query = graphql`
   query ArchitectTemplate($id: String!) {
     strapiArchitect(id: { eq: $id }) {
       id
+      name
+      bio
     }
   }
 `;
