d6a30b4fde501d737794da2864d1e0fc2d144cdc
Nick Railsback <nickrailsback@gmail.com>
2018-12-07 14:07:36 -0500

    styling home page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,14 +4,14 @@ import { Link } from 'gatsby'
 import './index.css'
 
 const IndexPage = () => (
-  <div className="row">
-    <div className="column">
-      <div>Yesterday</div>
+  <div className="row" id="index-frame">
+    <div className="column" id="yesterday">
+      <h1>Yesterday</h1>
       <div className="box"><Link to="/architects">Architects</Link></div>
       <div className="box"><Link to="/buildings">Buildings</Link></div>
     </div>
-    <div className="column">
-      <div>Today</div>
+    <div className="column" id="today">
+      <h1>Today</h1>
       <div className="box"><Link to="/posts">Blog</Link></div>
       <div className="box"><Link to="/events">Calendar</Link></div>
     </div>
