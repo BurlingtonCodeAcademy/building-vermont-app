55c16a2c8f932f3ef2661af97f17396c4d085d25
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-12-10 14:24:02 -0500

    removed unused import

--- client/src/pages/architects.js
+++ client/src/pages/architects.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
-import moment from 'moment';
 
 const ArchitectPage = ({ data }) => (
   <Layout>
