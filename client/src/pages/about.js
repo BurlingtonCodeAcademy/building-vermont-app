5b323cb9f2955661c3e4f996f93e243c99eddef4
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-12-11 17:43:07 -0500

    styling and update to about page

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -8,20 +8,31 @@ const AboutPage = () => (
         <h3>About</h3>
       </div>
       <div>
-        Vermont Modern is dedicated to the identification, documentation and
-        preservation of modernist architecture throughout Vermont. I began
-        researching Vermont's modernist architecture in 2004, as a graduate
+        <p>
+        Building Vermont is dedicated to the identification, documentation and
+        preservation of architecture throughout Vermont. I began
+        researching Vermont's architecture in 2004, as a graduate
         student in the Historic Preservation Graduate Program at the University
         of Vermont. My master's thesis documented modernist residential design
-        in Chittenden County, VT, and I continue to seek out modernist buildings
+        in Chittenden County, VT, and I continue to seek out buildings
         throughout the state and gather information on the architects who
-        designed them. The goal of Vermont Modern is to make all of the
+        designed them. 
+        </p>
+        <p>
+        The goal of Building Vermont is to make all of the
         information I have collected over the years available to students,
         researchers and the general public. Ideally, this information will raise
-        awareness of Vermont's modernist architectural resources and help
-        preserve these buildings for future generations to appreciate. Thanks
-        for your interest, and please feel free to contact me with any comments
-        or questions! Devin A. Colman Burlington, VT
+        awareness of Vermont's architectural resources and help
+        preserve these buildings for future generations to appreciate. 
+        </p>
+        <p>
+        Thanks for your interest, and please feel free to contact me with any comments
+        or questions! 
+        </p>
+        <p>
+        Devin A. Colman<br/>
+        Burlington, VT
+        </p>
       </div>
     </div>
   </Layout>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -7,11 +7,11 @@ const IndexPage = () => (
   <div className="container-image">
     <div className="index-row" id="index-frame">
         <h1>Building Vermont</h1>
-        <h4>The Past, Present, and Future of Vermont Architecture</h4>
-        <div className="box"><Link to="/architects"><h4>Architects</h4></Link></div>
-        <div className="box"><Link to="/buildings"><h4>Buildings</h4></Link></div>
-        <div className="box"><Link to="/posts"><h4>Blog</h4></Link></div>
-        <div className="box"><Link to="/events"><h4>Calendar</h4></Link></div>
+        <h3>The Past, Present, and Future<br/>of Vermont Architecture</h3>
+        <div className="box"><Link to="/architects"><h3>Architects</h3></Link></div>
+        <div className="box"><Link to="/buildings"><h3>Buildings</h3></Link></div>
+        <div className="box"><Link to="/posts"><h3>Blog</h3></Link></div>
+        <div className="box"><Link to="/events"><h3>Calendar</h3></Link></div>
       
     </div>
   </div>
