e6cb42144f4a27763d0aaf32a16be8f20c14d9aa
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-12-11 13:19:02 -0500

    dropdowns

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,9 +1,12 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import DropDownMenu from 'material-ui'
 import SearchBar from 'material-ui-search-bar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton'
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+
 
 
 const Header = ({ siteTitle }) => (
@@ -33,78 +36,70 @@ const Header = ({ siteTitle }) => (
         </h2>
       </div>
     </div>
-<h3>
-    <div
-      style={{
-        background: '#555',
-        marginBottom: '1.45rem',
-      }}
-    >
+    <h3>
       <div
         style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '.4rem .2rem',
+          background: '#555',
+          marginBottom: '1.45rem',
         }}
       >
-        <div style={{
-          margin: 0,
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-        }}>
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0 auto',
+          }}
+        >
           <div style={{
+            margin: 0,
+            display: 'flex',
+            alignItems: 'center',
           }}>
-            <Link
-              to="/architects"
-              style={{
-                color: 'white',
-                textDecoration: 'none',
-                padding: '.5rem',
-              }}
-            >
-              Yesterday
+                <div style={{color: '#fff', borderBottom: '0px solid white', alignSelf: 'center', paddingLeft: 20}}>Yesterday</div>
+            <div style={{padding: 0,}}>
+              <MuiThemeProvider>
+                <DropDownMenu underlineStyle={{borderTop: '0px'}}>
+                <MenuItem><a href="/architects">Architects</a></MenuItem>
+                <MenuItem><a href="/buildings">Buildings</a></MenuItem>
+                </DropDownMenu>
+              </MuiThemeProvider>
+            </div>
+            <div style={{color: '#fff',}}>Today</div>
+            <div style={{padding: 0, alignSelf: 'center'}}>
+            <MuiThemeProvider>
+            <DropDownMenu underlineStyle={{borderTop: '0px'}}>
+                <MenuItem><a href="/posts">Blog</a></MenuItem>
+                <MenuItem><a href="/events">Events</a></MenuItem>
+                </DropDownMenu>
+              </MuiThemeProvider>
+            </div>
+            <div style={{paddingLeft: 20,}}>
+              <MuiThemeProvider>
+                <SearchBar
+                  onChange={() => console.log('onChange')}
+                  onRequestSearch={() => console.log('onRequestSearch')}
+                  style={{
+                    margin: '0 auto',
+                    minWidth: 380,
+                    maxWidth: 800
+                  }}
+                />
+              </MuiThemeProvider>
+            </div>
+            <div style={{paddingLeft: 40,}}>
+              <Link
+                to="/about"
+                style={{
+                  color: 'white',
+                  textDecoration: 'none',
+                }}
+              >
+                About
         </Link>
-          </div>
-          <div>
-            <Link
-              to="/events"
-              style={{
-                display: 'flex',
-                color: 'white',
-                textDecoration: 'none',
-              }}
-            >
-              Today
-        </Link>
-          </div>
-          <div>
-          <MuiThemeProvider>
-          <SearchBar
-      onChange={() => console.log('onChange')}
-      onRequestSearch={() => console.log('onRequestSearch')}
-      style={{
-        margin: '0 auto',
-        minWidth: 380,
-        maxWidth: 800
-      }}
-    />
-  </MuiThemeProvider>
-          </div>
-          <div>
-            <Link
-              to="/about"
-              style={{
-                color: 'white',
-                textDecoration: 'none',
-              }}
-            >
-              About
-        </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </h3>
   </div>
 )
