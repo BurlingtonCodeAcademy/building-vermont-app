36b9da7bbf7380efd61601a248987662b79a9eb4
Leight Johnson <leightjohnson93@gmail.com>
2018-12-11 11:00:47 -0500

    remove dependency from gatsby-config

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -50,20 +50,5 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
-    {
-      resolve: `@gatsby-contrib/gatsby-plugin-elasticlunr-search`,
-      options: {
-        // Fields to index
-        fields: [`name`, `bio`],
-        // How to resolve each field`s value for a supported node type
-        resolvers: {
-          // For any node of type MarkdownRemark, list how to resolve the fields` values
-          MarkdownRemark: {
-            name: node => node.name,
-            bio: node => node.bio,
-          },
-        },
-      },
-    },
   ],
 };
