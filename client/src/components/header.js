01d74261fd3c7b4b5f46c3a567d26da54a7e6e83
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-12-11 15:06:26 -0500

    indentation

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -62,71 +62,71 @@ class Header extends Component {
   };
   render() {
     return (
-   <div
-    style={{
-      background: '#222',
-    }}
-  >
-    <div>
       <div
         style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '.6rem 1.0875rem',
+          background: '#222',
         }}
       >
-        <h2 style={{ margin: 0 }}>
-          <Link
-            to="/"
+        <div>
+          <div
             style={{
-              color: 'white',
-              textDecoration: 'none',
+              margin: '0 auto',
+              maxWidth: 960,
+              padding: '.6rem 1.0875rem',
             }}
           >
+            <h2 style={{ margin: 0 }}>
+              <Link
+                to="/"
+                style={{
+                  color: 'white',
+                  textDecoration: 'none',
+                }}
+              >
                 {this.props.siteTitle}
-          </Link>
-        </h2>
-      </div>
-    </div>
-    <h3>
-      <div
-        style={{
-          background: '#555',
-          marginBottom: '1.45rem',
-        }}
-      >
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0 auto',
-          }}
-        >
-          <div style={{
-            margin: 0,
-            display: 'flex',
-            alignItems: 'center',
-          }}>
-                <div style={{color: '#fff', borderBottom: '0px solid white', alignSelf: 'center', paddingLeft: 20}}>Yesterday</div>
-            <div style={{padding: 0,}}>
-              <MuiThemeProvider>
-                <DropDownMenu underlineStyle={{borderTop: '0px'}}>
-                <MenuItem><a href="/architects">Architects</a></MenuItem>
-                <MenuItem><a href="/buildings">Buildings</a></MenuItem>
-                </DropDownMenu>
-              </MuiThemeProvider>
-            </div>
-            <div style={{color: '#fff',}}>Today</div>
-            <div style={{padding: 0, alignSelf: 'center'}}>
-            <MuiThemeProvider>
-            <DropDownMenu underlineStyle={{borderTop: '0px'}}>
-                <MenuItem><a href="/posts">Blog</a></MenuItem>
-                <MenuItem><a href="/events">Events</a></MenuItem>
-                </DropDownMenu>
-              </MuiThemeProvider>
-            </div>
-            <div style={{paddingLeft: 20,}}>
-              <MuiThemeProvider>
+              </Link>
+            </h2>
+          </div>
+        </div>
+        <h3>
+          <div
+            style={{
+              background: '#555',
+              marginBottom: '1.45rem',
+            }}
+          >
+            <div
+              style={{
+                margin: '0 auto',
+                maxWidth: 960,
+                padding: '0 auto',
+              }}
+            >
+              <div style={{
+                margin: 0,
+                display: 'flex',
+                alignItems: 'center',
+              }}>
+                <div style={{ color: '#fff', borderBottom: '0px solid white', alignSelf: 'center', paddingLeft: 20 }}>Yesterday</div>
+                <div style={{ padding: 0, }}>
+                  <MuiThemeProvider>
+                    <DropDownMenu underlineStyle={{ borderTop: '0px' }}>
+                      <MenuItem><a href="/architects">Architects</a></MenuItem>
+                      <MenuItem><a href="/buildings">Buildings</a></MenuItem>
+                    </DropDownMenu>
+                  </MuiThemeProvider>
+                </div>
+                <div style={{ color: '#fff', }}>Today</div>
+                <div style={{ padding: 0, alignSelf: 'center' }}>
+                  <MuiThemeProvider>
+                    <DropDownMenu underlineStyle={{ borderTop: '0px' }}>
+                      <MenuItem><a href="/posts">Blog</a></MenuItem>
+                      <MenuItem><a href="/events">Events</a></MenuItem>
+                    </DropDownMenu>
+                  </MuiThemeProvider>
+                </div>
+                <div style={{ paddingLeft: 20, }}>
+                  <MuiThemeProvider>
                     <SearchBar
                       value={this.state.query}
                       onChange={input => {
@@ -146,25 +146,25 @@ class Header extends Component {
                         maxWidth: 800,
                       }}
                     />
-              </MuiThemeProvider>
-            </div>
-            <div style={{paddingLeft: 40,}}>
-              <Link
-                to="/about"
-                style={{
-                  color: 'white',
-                  textDecoration: 'none',
-                }}
-              >
-                About
+                  </MuiThemeProvider>
+                </div>
+                <div style={{ paddingLeft: 40, }}>
+                  <Link
+                    to="/about"
+                    style={{
+                      color: 'white',
+                      textDecoration: 'none',
+                    }}
+                  >
+                    About
         </Link>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        </h3>
       </div>
-    </h3>
-  </div>
-   );
+    );
   }
 }
 
