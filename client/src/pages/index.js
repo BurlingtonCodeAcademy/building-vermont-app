166c0af1a7e5ea5220be6d4f951364cc40f710bf
Nick Railsback <nickrailsback@gmail.com>
2018-12-11 16:29:13 -0500

    plz help with background image, css grid needs work

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,16 +4,15 @@ import { Link } from 'gatsby'
 import './index.css'
 
 const IndexPage = () => (
-  <div className="index-row" id="index-frame">
-    <div className="index-column" id="yesterday">
-      <h1>Yesterday</h1>
-      <div className="box"><Link to="/architects">Architects</Link></div>
-      <div className="box"><Link to="/buildings">Buildings</Link></div>
-    </div>
-    <div className="index-column" id="today">
-      <h1>Today</h1>
-      <div className="box"><Link to="/posts">Blog</Link></div>
-      <div className="box"><Link to="/events">Calendar</Link></div>
+  <div className="container-image">
+    <div className="index-row" id="index-frame">
+        <h1>Building Vermont</h1>
+        <h4>The Past, Present, and Future of Vermont Architecture</h4>
+        <div className="box"><Link to="/architects">Architects</Link></div>
+        <div className="box"><Link to="/buildings">Buildings</Link></div>
+        <div className="box"><Link to="/posts">Blog</Link></div>
+        <div className="box"><Link to="/events">Calendar</Link></div>
+      
     </div>
   </div>
 )
