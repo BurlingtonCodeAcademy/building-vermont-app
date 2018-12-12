b9975f422c9c94a43638e052e5f7a2636f6ba402
Habenzy <robertrstauss@gmail.com>
2018-12-12 16:21:58 -0500

    changed calendar name to event

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
       <div className="box" id="one"><Link to="/architects"><h4>Architects</h4></Link></div>
       <div className="box" id="two"><Link to="/buildings"><h4>Buildings</h4></Link></div>
       <div className="box" id="three"><Link to="/posts"><h4>Blog</h4></Link></div>
-      <div className="box" id="four"><Link to="/events"><h4>Calendar</h4></Link></div>
+      <div className="box" id="four"><Link to="/events"><h4>Events</h4></Link></div>
       <h1 className="index-row" id="five">Building<br /> Vermont</h1><hr/>
       
       <h3 className="index-row" id="six">The Past, Present, and Future<br />of Vermont Architecture</h3>
