48ee06b6887881dc5ade107946722cb5014632c7
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-12-13 14:37:37 -0500

    added lists of buildings to years page

--- client/src/pages/years.js
+++ client/src/pages/years.js
@@ -6,8 +6,19 @@ import './index.css'
 function uniqueYear(arr) {
   if (arr.length === 0) return arr;
   let ret = [arr[0]];
-  for (var i = 1; i < arr.length; i++) { //Start loop at 1: arr[0] can never be a duplicate
-    if (arr[i-1].node.year !== arr[i].node.year) {
+  for (let i = 1; i < arr.length; i++) { //Start loop at 1: arr[0] can never be a duplicate
+    if (arr[i - 1].node.year !== arr[i].node.year) {
+      ret.push(arr[i]);
+    }
+  }
+  return ret;
+}
+
+function builtInYear(arr, year) {
+  if (arr.length === 0) return arr;
+  let ret = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (year === arr[i].node.year) {
       ret.push(arr[i]);
     }
   }
@@ -20,9 +31,11 @@ const YearPage = ({ data }) => (
     <ul>
       {(uniqueYear(data.allStrapiBuilding.edges)).map(document => (
         <li key={document.node.id}>
-          <h3>
-            <Link to={`/buildings/${(document.node.year)}`}>{document.node.year}</Link>
-          </h3>
+          <div className="row">
+            <h3 style={{marginRight: 4}}><Link to={`/buildings/${(document.node.year)}`}>{document.node.year}</Link></h3>
+            <div>{(builtInYear(data.allStrapiBuilding.edges, document.node.year)).map(document => (" - " + document.node.name))}
+            </div>
+          </div>
         </li>
       ))}
     </ul>
@@ -41,6 +54,7 @@ export const pageQuery = graphql`
       edges {
         node {
           id
+          name
           year
             }
       }
