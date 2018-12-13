9c67ae9fe0df5728718f6c042d1b7906530bc507
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-12-13 15:11:58 -0500

    styling

--- client/src/pages/posts.js
+++ client/src/pages/posts.js
@@ -12,12 +12,14 @@ const PostPage = ({ data }) => (
         <li key={document.node.id}>
           <p>{moment(document.node.date).format('MMMM Do, YYYY')}</p>
           <h2><Link to={`/posts/${document.node.id}`}>{document.node.title}</Link></h2>
-
+          <br />
           <div
             dangerouslySetInnerHTML={{ __html: marked(document.node.body) }}
           />
+          <br />
           <p>Posted by: {document.node.author}</p>
           <hr />
+          <br />
         </li>
       ))}
     </ul>
