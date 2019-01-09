488ad32e27fa04a2b3ad33f64a6ff7526dab1ba0
Habenzy <robertrstauss@gmail.com>
2019-01-09 14:02:41 -0500

    changed year order to Ascending

--- client/src/pages/years.js
+++ client/src/pages/years.js
@@ -53,7 +53,7 @@ export const pageQuery = graphql`
   query YearQuery {
     allStrapiBuilding(
       sort: {
-        fields: [ year ], order: DESC
+        fields: [ year ], order: ASC
       }
     ) {
       edges {
